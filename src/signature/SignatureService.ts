b7b6147cd81a8b9bbe2da0e9d8833eb94ced77b3
Jose Ramon Vieites Romay <jose.vieites@altia.es>
2022-07-31 05:17:12 +0200

    Save image implemented

--- src/signature/SignatureService.ts
+++ src/signature/SignatureService.ts
@@ -9,9 +9,34 @@ export class SignatureService {
     this.signaturePad = new SignaturePad(htmlElement)
   }
 
+  private dataURLToBlob(dataUrl: string): Blob {
+    const parts = dataUrl.split(';base64,')
+    const contentType = parts[0].split(":")[1]
+    const raw = window.atob(parts[1])
+    const rawLength = raw.length;
+    const uInt8Array = new Uint8Array(rawLength);
+
+    for (let i = 0; i < rawLength; ++i) {
+      uInt8Array[i] = raw.charCodeAt(i);
+    }
+      return new Blob([uInt8Array],  {type: contentType})
+  }
+
+  private download(dataUrl: string, fileName) {
+    const blob = this.dataURLToBlob(dataUrl)
+    const url = window.URL.createObjectURL(blob)
+    const a = document.createElement("a")
+    a.style.display = "none"
+    a.href = url
+    a.download = fileName
+    document.body.appendChild(a)  
+    a.click()
+    window.URL.revokeObjectURL(url)
+  }
+
   savePNG() {
     if (!this.signaturePad.isEmpty()) {
-      return this.signaturePad.toDataURL()
+      this.download(this.signaturePad.toDataURL(), "signature.png")
     }
   } 
 
