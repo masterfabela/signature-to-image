fe97c4a04b842d4ced101c73cd648b9ef78ca5a2
Jose Ramon Vieites Romay <jose.vieites@altia.es>
2022-07-31 05:42:22 +0200

    Deployed in firebase

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
+  import Actions from '@smui/card/src/Actions.svelte';
   import Layout from './Layout.svelte';
+  import { initializeApp } from "firebase/app";
+  import { getAnalytics } from "firebase/analytics";  
+  import firebaseConfig from '../firebase.json';
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app)
+  
 </script>
+
 
 <main>
   <Layout />
